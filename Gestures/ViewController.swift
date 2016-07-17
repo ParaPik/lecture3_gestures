898addb0adefcd8e199b4e0433a6dc786114118c
kamiraider <pazzl@icloud.com>
2016-07-17 17:01:21 +0300

    Add gestures

--- Gestures/ViewController.swift
+++ Gestures/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.panGesture(_:)))
+        let scaleGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.scaleGesture(_:)))
+        
+        imageView.gestureRecognizers = [panGesture, scaleGesture]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func scaleGesture(gesture : UIPinchGestureRecognizer) {
+        switch gesture.state {
+        case .Changed, .Ended:
+            let scale = gesture.scale
+            let transform = CGAffineTransformMakeScale(scale, scale)
+            let frame = CGRectApplyAffineTransform(imageView.bounds, transform)
+            imageView.bounds = frame
+            
+            gesture.scale = 1
+        default: break
+        }
+    }
+    
+    func panGesture(gesture : UIPanGestureRecognizer) {
+        switch gesture.state {
+            case .Changed, .Ended:
+            let translation = gesture.translationInView(imageView)
+            imageView.center = CGPoint(
+                x: imageView.center.x + translation.x,
+                y: imageView.center.y + translation.y)
+            gesture.setTranslation(CGPointZero, inView: imageView)
+            
+            default: break
+        }
+    }
+    
+    @IBAction func swipeGesture(sender: UISwipeGestureRecognizer) {
+        performSegueWithIdentifier("swipeSegue", sender: nil)
+    }
 }
 
